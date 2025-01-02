895d0ec98e59e9a6b023c7002a0f02249e1c805a
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-01-02 12:54:30 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,39 +6,6 @@
 
 (function($) {
 
-var myJSON= {"myObject": {
-"JAVA": {
-    "id": "q",
-    "path": "json/data.json",
-    "posx": "a0",
-    "posy": "a0"
-},
-"C#": { 
-    "id": "w",
-    "path": "json/data1.json",
-    "posx": "a1",
-    "posy": "a0"
-},
-"C++": {
-   "id": "e",
-   "path": "json/data2.json",
-   "posx": "a2", 
-   "posy": "a0"
-}
-}
-}
-
-
-var dctLanguages = myJSON["myObject"];
-var container= document.getElementById('buttons');
-for (var key in dctLanguages)
-{
-   var language = dctLanguages[key];
-   var button ='<div class="key" id="'+language.id+'" value="'+key+'"/>';
-   container.innerHTML+=button;
-   button.top = "10px"; 
-   button.style.marginLeft = parseInt(language.posx) * 100px; 
-}
 	
 	var	$window = $(window),
 		$body = $('body'),
