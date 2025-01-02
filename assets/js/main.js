440cf07cbaa8fc385200c199bdac5a0815661c46
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-01-02 12:41:30 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ for (var key in dctLanguages)
    var button ='<div class="key" id="'+language.id+'" value="'+key+'"/>';
    container.innerHTML+=button;
    button.top = "10px"; 
-   button.marginLeft = "80px"; //= parseInt(language.posx); 
+   button.marginLeft = parseInt(language.posx) * 100; 
 }
 	
 	var	$window = $(window),
