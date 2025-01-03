2618d8127680949dde86faeeeb443305d11f2c1b
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-01-02 21:57:29 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,8 @@
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
 	 */
+	 alert("let's go!");
+	
 	$.fn._parallax = function(intensity) {
 
 		var	$window = $(window),
