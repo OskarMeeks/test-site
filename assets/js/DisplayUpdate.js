c9ec263bc0f40a6dea2a93a8ec017cc78f46e4d7
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-01-04 23:21:28 -0700

    Update DisplayUpdate.js

--- assets/js/DisplayUpdate.js
+++ assets/js/DisplayUpdate.js
@@ -5,7 +5,7 @@ var brush = "grey";
 var aspect = "color";
 
 function setbrush(brsh, aspct) {
-brush = inp;
+brush = brsh;
 aspect = aspct;
 
 		 alert("brush changed");
@@ -71,7 +71,7 @@ var myJSON= {"myObject": {
  		  button.classList.add("key"); 
  		  button.id = language.id; // Set the id attribute
 		  button.setAttribute("value", key); 
-		//  button.addEventListener("click", update("false", "true", language.id));
+		  button.addEventListener("click", update("false", "true", language.id));
 
 		
   		  button.style.top = "10px"; 
