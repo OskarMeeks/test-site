64fe0ca477b23e128aacdb6d9f267e7c84e20e25
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-01-05 22:45:40 -0700

    Update DisplayUpdate.js

--- assets/js/DisplayUpdate.js
+++ assets/js/DisplayUpdate.js
@@ -78,7 +78,7 @@ function update(reload, jsonedit, key_) {
 		    // Add the button to the container
   		  container.appendChild(button); 
 		  var content = document.createElement("label");
-		  content.value = "test";
+		  content.textContent=curentkey.id.toString();
 		  button.appendChild(content);
 	      }
 			//edit style of one    
