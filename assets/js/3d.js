2942f0b14d522fed1a443e0662b2f797d51a2d66
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-02-16 10:28:58 -0700

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -1,95 +1,137 @@
-//Import the THREE.js library
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// To allow for the camera to move around the scene
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// To allow for importing the .gltf file
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
+//  `./models/${objToRender}/scene.gltf`
 
-//Create a Three.JS Scene
-const scene = new THREE.Scene();
-//create a new camera with positions and angles
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+			import * as THREE from 'three';
 
-//Keep track of the mouse position, so we can make the eye move
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
+			import Stats from 'three/addons/libs/stats.module.js';
+			import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-//Keep the 3D object on a global variable so we can access it later
-let object;
+			let camera, scene, renderer, controls, stats;
 
-//OrbitControls allow the camera to move around the scene
-let controls;
+			let mesh;
+			const amount = parseInt( window.location.search.slice( 1 ) ) || 10;
+			const count = Math.pow( amount, 3 );
 
-//Set which object to render
-let objToRender = 'dino';
+			const raycaster = new THREE.Raycaster();
+			const mouse = new THREE.Vector2( 1, 1 );
 
-//Instantiate a loader for the .gltf file
-const loader = new GLTFLoader();
+			const color = new THREE.Color();
+			const white = new THREE.Color().setHex( 0xffffff );
 
-//Load the file
-loader.load(
-  `./models/${objToRender}/scene.gltf`,
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    object = gltf.scene;
+			init();
 
-    //object.material.color.setHex(0xff0000);
-    camera.position.z = 5;
-	  
-    scene.add(object);
+			function init() {
 
+				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+				camera.position.set( amount, amount, amount );
+				camera.lookAt( 0, 0, 0 );
 
-							
-  }
-);
-//innit();
-//function innit(){
-	//Instantiate a new renderer and set its size
-	const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
-	renderer.setSize(window.innerWidth, window.innerHeight);
+				scene = new THREE.Scene();
 
-	//Add the renderer to the DOM
-	document.getElementById("container3D").appendChild(renderer.domElement);
+				const light = new THREE.HemisphereLight( 0xffffff, 0x888888, 3 );
+				light.position.set( 0, 1, 0 );
+				scene.add( light );
 
-	//Set how far the camera will be from the 3D model
-	camera.position.z = objToRender === "dino" ? 25 : 500;
+				const geometry = new THREE.IcosahedronGeometry( 0.5, 3 );
+				const material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
 
-	//Add lights to the scene, so we can actually see the 3D model
-	const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-	topLight.position.set(500, 500, 500) //top-left-ish
-	topLight.castShadow = true;
-	scene.add(topLight);
+				mesh = new THREE.InstancedMesh( geometry, material, count );
 
-	const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-	scene.add(ambientLight);
+				let i = 0;
+				const offset = ( amount - 1 ) / 2;
 
+				const matrix = new THREE.Matrix4();
 
-	//This adds controls to the camera, so we can rotate / zoom it with the mouse
-	if (objToRender === "dino") {
-	  controls = new OrbitControls(camera, renderer.domElement);
-	}
-//}
-//Render the scene
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
+				for ( let x = 0; x < amount; x ++ ) {
 
-//Add a listener to the window, so we can resize the window and the camera
-window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
+					for ( let y = 0; y < amount; y ++ ) {
 
-//add mouse position listener, so we can make the eye move
-document.onmousemove = (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-}
+						for ( let z = 0; z < amount; z ++ ) {
 
-//Start the 3D rendering
-animate();
+							matrix.setPosition( offset - x, offset - y, offset - z );
 
+							mesh.setMatrixAt( i, matrix );
+							mesh.setColorAt( i, color );
 
+							i ++;
+
+						}
+
+					}
+
+				}
+
+				scene.add( mesh );
+
+				//
+
+				const gui = new GUI();
+				gui.add( mesh, 'count', 0, count );
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.setAnimationLoop( animate );
+				document.body.appendChild( renderer.domElement );
+
+				controls = new OrbitControls( camera, renderer.domElement );
+				controls.enableDamping = true;
+				controls.enableZoom = false;
+				controls.enablePan = false;
+
+				stats = new Stats();
+				document.body.appendChild( stats.dom );
+
+				window.addEventListener( 'resize', onWindowResize );
+				document.addEventListener( 'mousemove', onMouseMove );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function onMouseMove( event ) {
+
+				event.preventDefault();
+
+				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+			}
+
+			function animate() {
+
+				controls.update();
+
+				raycaster.setFromCamera( mouse, camera );
+
+				const intersection = raycaster.intersectObject( mesh );
+
+				if ( intersection.length > 0 ) {
+
+					const instanceId = intersection[ 0 ].instanceId;
+
+					mesh.getColorAt( instanceId, color );
+
+					if ( color.equals( white ) ) {
+
+						mesh.setColorAt( instanceId, color.setHex( Math.random() * 0xffffff ) );
+
+						mesh.instanceColor.needsUpdate = true;
+
+					}
+
+				}
+
+				renderer.render( scene, camera );
+
+				stats.update();
+
+			}
