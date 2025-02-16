81013b3fe72300c4c7c149fa3e51f40772e3166a
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-02-15 23:48:43 -0700

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -32,23 +32,38 @@ loader.load(
   `./models/${objToRender}/scene.gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
-    object = gltf.scene;
+  //  object = gltf.scene;
 
 
-
-camera.position.z = 5;
+	//camera.position.z = 5;
 	  
-    scene.add(object);
+//    scene.add(object);
+	  gltf.scene.traverse( function ( child ) {
 
-	 object.material.color.set(new THREE.Color(0xff0000)); // Sets the color to red (hexadecimal)
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
+						if ( child.isMesh ) {
+
+							// glTF currently supports only tangent-space normal maps.
+							// this model has been modified to demonstrate the use of an object-space normal map.
+
+							child.material.normalMapType = THREE.ObjectSpaceNormalMap;
+
+							// attribute normals are not required with an object-space normal map. remove them.
+
+							child.geometry.deleteAttribute( 'normal' );
+
+							//
+
+							child.material.side = THREE.DoubleSide;
+
+							child.scale.multiplyScalar( 0.5 );
+
+							// recenter
+
+							new THREE.Box3().setFromObject( child ).getCenter( child.position ).multiplyScalar( - 1 );
+
+							scene.add( child );
+
+							
   }
 );
 //innit();
@@ -71,6 +86,7 @@ camera.position.z = 5;
 
 	const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
 	scene.add(ambientLight);
+	 object.material.color.setHex(0xff0000); // Sets the color to red (hexadecimal)
 
 	//This adds controls to the camera, so we can rotate / zoom it with the mouse
 	if (objToRender === "dino") {
