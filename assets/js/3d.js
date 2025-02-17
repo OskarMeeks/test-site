e052fea49b004f29293d0365230c5bef483463f2
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-02-17 14:36:31 -0700

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -101,8 +101,8 @@
 
 				event.preventDefault();
 
-				mouse.x = ( event.pageX / window.innerWidth);
-				mouse.y = - ( event.pageY / window.innerHeight);
+				mouse.x = ( event.pageX / window.innerWidth) * 2 - 1;
+				mouse.y = - ( event.pageY / window.innerHeight) * 2 + 1;
 
 			}
 
