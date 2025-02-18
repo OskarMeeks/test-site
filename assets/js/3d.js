218214ad1f916d5c4a3d5c1e2ccdf2fcd8081a86
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-02-18 15:49:18 -0700

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -205,7 +205,7 @@ loader.load(
 
 			}
 
-var brush = "0xaaffaa";
+var brush = "#aaffaa";
 var aspect1 = "color";
 var aspect2;
 
@@ -217,7 +217,7 @@ aspect2 = aspct2;
 
 //called when clicking on a key to quick apply a style     uses saved value and aspect, imediately defined key
 function update(keyid, keylocation){
-	mesh.setColorAt(keyid, new THREE.Color(myJSON["myObject"][keylocation].color) );  
+	mesh.setColorAt(keyid, myJSON["myObject"][keylocation].color );  
 	//mesh.setColorAt( clickedkey, color.setHex(0xaaffaa )) ;
 	mesh.instanceColor.needsUpdate = true;
 	console.log(myJSON["myObject"][keylocation].color);
