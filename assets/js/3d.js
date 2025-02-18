37a816e90da52481e348d259ece21e90d516fcfc
OskarMeeks <126992079+OskarMeeks@users.noreply.github.com>
2025-02-18 00:48:20 +0000

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -90,7 +90,7 @@ loader.load(
 
 				mesh = new THREE.InstancedMesh( geometry, material, count );
 				
-					mesh.rotation.x = 0;
+					mesh.rotation.x = Math.PI;
 					mesh.rotation.y = Math.PI / 2;
 					mesh.rotation.z = (Math.PI / 2)*3;
 				
